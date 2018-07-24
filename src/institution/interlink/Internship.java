e46121414c3dbe9a509ec761159b7a23773b324c
Znak01 <36077389+Znak01@users.noreply.github.com>
2018-07-24 11:55:01 +0300

    Update Internship.java

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -7,8 +7,8 @@ import person.Student;
 
 public class Internship {
 	
-	private String name;
-	private List<Student> students;
+    private String name;
+    private List<Student> students;
 	
     public Internship(String name) {
        this.name = name;
@@ -16,23 +16,23 @@ public class Internship {
 
     public String getName() {
 		return name;
-	}
-	public void setName(String name) {
+    }
+    public void setName(String name) {
 		this.name = name;
-	}
+    }
 
-	public void setStudent(Student student) {
-        if(students == null) {
-        	students = new ArrayList<>();
+    public void setStudent(Student student) {
+       if(students == null) {
+           students = new ArrayList<>();
         }
-        students.add(student);
+           students.add(student);
     }
 
     public String getStudents() {
         StringBuilder sb = new StringBuilder();
         for (Student student : students) {
-			sb.append(student.getName()+"\n");
-		}
+		sb.append(student.getName()+"\n");
+	  }
         String list = sb.toString();
         return list;
     }
